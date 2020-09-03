eed44a3d7d765b92e358c72a592cbce0f8baedd7
wxingda <vip.da@qq.com>
2020-09-03 18:50:32 +0800

    remove useless codes

--- vector/vector_manager.cc
+++ vector/vector_manager.cc
@@ -396,9 +396,6 @@ int VectorManager::Search(GammaQuery &query, GammaResult *results) {
         all_vector_results[i].sort_by_docid();
       }
     }
-    if (query.condition->sort_by_docid) {
-      all_vector_results[i].sort_by_docid();
-    }
 #ifdef PERFORMANCE_TESTING
     std::string msg;
     msg += "search " + std::to_string(i);
